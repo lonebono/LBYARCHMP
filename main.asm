9d9871250c85d7193dab1ef85db3e84edd048cd5
lonebono <vaughnsy15@gmail.com>
2025-11-24 19:15:18 +0800

    added comments for clarity

--- main.asm
+++ main.asm
@@ -5,21 +5,23 @@ formula  DQ 0.003921568627451 ; precomputed
 
 section .text
 imgCvtGrayIntToDouble:
-    TEST R8, R8
-    JZ .RET
+    TEST R8, R8 ; checks if pixel count is zero
+    JZ .RET ; exits if zero
 
-    MOVSD XMM1, [REL formula]
+    MOVSD XMM1, [REL formula]  ; load precomputed formula
 
 .LOOP:
-    MOVZX EAX, BYTE [RCX]
-    CVTSI2SD XMM0, EAX
-    MULSD XMM0, XMM1
-    MOVSD [RDX], XMM0
+    MOVZX EAX, BYTE [RCX] ; load pixel value
+    CVTSI2SD XMM0, EAX ; convert to double
+    MULSD XMM0, XMM1 ; multiply by formula
+    MOVSD [RDX], XMM0 ; store result
 
+    ; advance pointer and decrement pixel count
     ADD RCX, 1
     ADD RDX, 8
     DEC R8
     JNZ .LOOP
 
+    ; ends the loop
 .RET:
     RET
